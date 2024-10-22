8aced714d695c8969e86bb49e9ce336d741d03b9
Sahal V A <sahal.v@axelerant.com>
2024-10-22 06:35:43 +0530

    Exercise: TodoListItem class component with child and isComplete props

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,8 +1,15 @@
-export function TodoListItem({ children, isComplete }) {
-  return (
-    <div>
-      <input type="checkbox" id="is_complete" checked={isComplete} />
-      <label htmlFor="is_complete">{children}</label>
-    </div>
-  )
+import React from "react"
+export class TodoListItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          id="is_complete"
+          checked={this.props.isComplete}
+        />
+        <label htmlFor="is_complete">{this.props.children}</label>
+      </div>
+    )
+  }
 }
