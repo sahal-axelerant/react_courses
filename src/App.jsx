79332b563e66434f4e3ccb2322ed94485c67d3d2
Sahal V A <sahal.v@axelerant.com>
2024-10-22 07:42:56 +0530

    Exercise: 15 - User Card function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "./user.css"
+import userJson from "./user.json"
+import { UserCard } from "./UserCard"
+
 function App() {
-  return "Hello World"
+  return (
+    <UserCard
+      name={userJson.name}
+      age={userJson.age}
+      phoneNumber={userJson.phoneNumber}
+      address={userJson.address}
+    />
+  )
 }
 
 export default App
