3b38d777d910a8d631a97dd5f9983f5d1fc290e3
Sahal V A <sahal.v@axelerant.com>
2024-12-09 00:13:11 +0530

    Exercise: Basic form Validation using React hook form lib

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,30 +1,22 @@
-import { useMemo } from "react"
-import { useRef } from "react"
-import { useState } from "react"
-import { checkEmail, checkPassword } from "./Validators"
+import { useForm } from "react-hook-form"
 
 export function LoginForm() {
-  const [isFirstSubmitDone, setIsFirstSubmitDone] = useState(false)
-  const [emailErrors, setEmailErrors] = useState([])
-  const [passwordErrors, setPasswordErrors] = useState([])
-  const email = useRef()
-  const password = useRef()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { email: "sahal.v@axelerant.com" } })
 
-  function handleFormSubmit(e) {
-    e.preventDefault()
-    setIsFirstSubmitDone(true)
-    const emailResults = checkEmail(email.current.value)
-    const passwordResults = checkPassword(password.current.value)
-    if (emailResults.length === 0 && passwordResults.length === 0) {
-      alert("Success")
-    } else {
-      setEmailErrors(emailResults)
-      setPasswordErrors(passwordResults)
-    }
+  function handleFormSubmit(data) {
+    alert("Success")
   }
   return (
-    <form onSubmit={handleFormSubmit} className="form">
-      <div className={`form-group ${emailErrors.length > 0 ? "error" : ""}`}>
+    <form onSubmit={handleSubmit(handleFormSubmit)} className="form">
+      <div
+        className={`form-group ${
+          errors?.email?.message.length > 0 ? "error" : ""
+        }`}
+      >
         <label className="label" htmlFor="email">
           Email
         </label>
@@ -32,18 +24,24 @@ export function LoginForm() {
           className="input"
           type="email"
           id="email"
-          ref={email}
-          onChange={(e) =>
-            isFirstSubmitDone
-              ? setEmailErrors(checkEmail(email.current.value))
-              : undefined
-          }
+          {...register("email", {
+            required: { value: true, message: "Required" },
+            validate: (value) => {
+              if (!value.endsWith("@webdevsimplified.com")) {
+                return "Must end with @webdevsimplified.com"
+              }
+            },
+          })}
         />
-        {emailErrors.length > 0 && (
-          <div className="msg">{emailErrors.join(", ")}</div>
+        {errors?.email?.message.length > 0 && (
+          <div className="msg">{errors.email.message}</div>
         )}
       </div>
-      <div className={`form-group ${passwordErrors.length > 0 ? "error" : ""}`}>
+      <div
+        className={`form-group ${
+          errors?.password?.message.length > 0 ? "error" : ""
+        }`}
+      >
         <label className="label" htmlFor="password">
           Password
         </label>
@@ -51,15 +49,34 @@ export function LoginForm() {
           className="input"
           type="text"
           id="password"
-          ref={password}
-          onChange={(e) =>
-            isFirstSubmitDone
-              ? setPasswordErrors(checkPassword(password.current.value))
-              : undefined
-          }
+          {...register("password", {
+            required: { value: true, message: "Required" },
+            validate: {
+              hasMinCount: (value) => {
+                if (value.length < 10) {
+                  return "Must be at least 10 characters"
+                }
+              },
+              hasLowerCase: (value) => {
+                if (!value.match(/[a-z]/)) {
+                  return "Must include at least 1 lowercase letter"
+                }
+              },
+              hasUppercase: (value) => {
+                if (!value.match(/[A-Z]/)) {
+                  return "Must include at least 1 uppercase letter"
+                }
+              },
+              hasNumber: (value) => {
+                if (!value.match(/[0-9]/)) {
+                  return "Must include at least 1 number"
+                }
+              },
+            },
+          })}
         />
-        {passwordErrors.length > 0 && (
-          <div className="msg">{passwordErrors.join(", ")}</div>
+        {errors?.password?.message.length > 0 && (
+          <div className="msg">{errors.password.message}</div>
         )}
       </div>
       <button className="btn" type="submit">
